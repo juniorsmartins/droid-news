223fc3f0f439cf162f7a86573b63876078bf30c0
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-08-26 22:39:02 -0400

    FEAT: add microservice de WebScraping.

---
+++ droid-webscraping/src/main/java/io/droidwebscraping/DroidWebscrapingApplication.java
@@ -0,0 +1,13 @@
+package io.droidwebscraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroidWebscrapingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroidWebscrapingApplication.class, args);
+	}
+
+}
