29763dcd076fd1470e0f9939fb4cc61597f12cfe
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-08-26 23:13:00 -0400

    ...

--- droid-webscraping/src/main/java/io/droidwebscraping/DroidWebscrapingApplication.java
+++ droid-webscraping/src/main/java/io/droidwebscraping/DroidWebscrapingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DroidWebscrapingApplication {
 
+	private static final String URL_MINISTERIO_ESPORTE = "https://www.gov.br/esporte/pt-br";
+
 	public static void main(String[] args) {
 		SpringApplication.run(DroidWebscrapingApplication.class, args);
 	}
