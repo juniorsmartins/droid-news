c5750e833c1c4324874a40ec71a7f4c2ca61a14d
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-08-27 14:25:01 -0400

    FEAT: conexão com link de notícias do Ministério do Esporte.

--- droid-webscraping/src/main/java/io/droidwebscraping/DroidWebscrapingApplication.java
+++ droid-webscraping/src/main/java/io/droidwebscraping/DroidWebscrapingApplication.java
@@ -3,13 +3,17 @@ package io.droidwebscraping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.droidwebscraping.services.ServiceScrapingMinisterioEducacao;
+
 @SpringBootApplication
 public class DroidWebscrapingApplication {
 
-	private static final String URL_MINISTERIO_ESPORTE = "https://www.gov.br/esporte/pt-br";
-
 	public static void main(String[] args) {
 		SpringApplication.run(DroidWebscrapingApplication.class, args);
+
+		var scraping = new ServiceScrapingMinisterioEducacao();
+		scraping.obtainNews();
 	}
 
 }
+
