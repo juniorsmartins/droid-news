95e79eeb6692770efe35510b9bf38b3c523ff306
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-02 16:01:11 -0400

    REFACTOR: resolvido conflito de dependências.

--- droid-gateway/src/main/java/io/droidnews/droidgateway/DroidGatewayApplication.java
+++ droid-gateway/src/main/java/io/droidnews/droidgateway/DroidGatewayApplication.java
@@ -2,6 +2,7 @@ package io.droidnews.droidgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
