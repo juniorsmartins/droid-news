98c4a1434fdebce4fbbbc0815bbe51a5d66d854b
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-02 19:19:30 -0400

    FEAT: funciona.

--- droid-news-users/src/main/java/oi/droidnewsusers/interface_adapters/controllers/UserControllerDeleteByIdImpl.java
+++ droid-news-users/src/main/java/oi/droidnewsusers/interface_adapters/controllers/UserControllerDeleteByIdImpl.java
@@ -1,17 +1,18 @@
 package oi.droidnewsusers.interface_adapters.controllers;
 
-import oi.droidnewsusers.application_business_rules.use_cases.UserServiceDeleteById;
-import oi.droidnewsusers.interface_adapters.gateways.UserGatewayBuscarPorId;
-import oi.droidnewsusers.interface_adapters.gateways.UserGatewayDelete;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import oi.droidnewsusers.application_business_rules.use_cases.UserServiceDeleteById;
+import oi.droidnewsusers.interface_adapters.gateways.UserGatewayBuscarPorId;
+import oi.droidnewsusers.interface_adapters.gateways.UserGatewayDelete;
 
 @RestController
-@RequestMapping(path = "/api/v1/users")
+@RequestMapping
 public final class UserControllerDeleteByIdImpl implements UserController.UserControllerDeleteById {
 
   private final UserServiceDeleteById userServiceDeleteById;
