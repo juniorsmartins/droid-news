afef62b0fcb40a80af423376fea0a9b11eae2e3b
juniorsmartins <juniorsoaresmartins@gmail.com>
2023-09-02 14:27:22 -0400

    FEAT: add Spring Cloud Gateway.

---
+++ droid-gateway/src/main/java/io/droidnews/droidgateway/DroidGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.droidnews.droidgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroidGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroidGatewayApplication.class, args);
+	}
+
+}
